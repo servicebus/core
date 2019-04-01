dba97fbb121b448d6301b456e81e77927ded60f3
Patrick Scott <pat@patscott.io>
2019-03-31 23:13:09 -0400

    chore: cleanup

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-const Bus = require('./lib/index');
+const Bus = require('./lib/bus');
 const BusProxy = require('./lib/proxy');
 
 module.exports = Bus;
 module.exports.BusProxy = BusProxy;
-
-console.log(module.exports)
